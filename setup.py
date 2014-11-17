7ad8848c3966c406b168a537f369163df3cb57c0
Jocelyn Delalande <jdelalande@oasiswork.fr>
2014-11-17 17:15:56 +0100

    version bump to 0.2 and stop using commit id as part of version

--- setup.py
+++ setup.py
@@ -6,17 +6,8 @@ from setuptools import setup
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
-
-def mk_version(base_version):
-    try:
-        GIT_HEAD = open('.git/HEAD').readline().split(':')[1].strip()
-        GIT_HEAD_REV = open('.git/{}'.format(GIT_HEAD)).readline().strip()
-        return '{}-git-{:.7}'.format(base_version, GIT_HEAD_REV)
-    except IOError:
-        return base_version
-
 setup(name='copiste',
-      version=mk_version('0.1'),
+      version='0.2',
       description='PostgreSQL to LDAP realtime sync tools',
       long_description=README,
       author='Jocelyn Delalande',
